59703d355b7886785eca7dcedd6badde3f96cc39
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-25 04:11:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,14 +10,14 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, amount)
-  array.pop(amount)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift
 end
 
-def shift_with_args(array, amount)
-  array.shift(amount)
+def shift_with_args(array)
+  array.shift(2)
 end
